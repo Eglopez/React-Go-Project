6bebf941580490b08c0eaf39743484c83c6950a5
Eduardo López <eduardolopezlainez2001@gmail.com>
2021-05-18 19:20:51 -0600

    Submit Login

--- frontend/src/components/LoginInput/index.js
+++ frontend/src/components/LoginInput/index.js
@@ -1,30 +1,25 @@
 import React from 'react';
 import { Form, Col, Row } from 'react-bootstrap';
 
-export default function Input({ setState, state }){
+export default function Input({ setState, state, type, placeholder, controlId, label }){
 
+    
     const handleUser = (e) => {
-        setState({...state, [e.target.name]: e.target.value})
+        console.log(e.target.value);
+        setState({...state, [label]: e.target.value})
     }
 
     return(
         <>
-            <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Group as={Row} controlId={ controlId }>
                 <Form.Label column sm="2">
-                    Username
+                    { label }
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control type="text" placeholder="@username" onChange={handleUser}/>
+                    <Form.Control type={ type } placeholder={ placeholder }  onChange={ handleUser } />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-                <Form.Label column sm="2">
-                    Password
-                </Form.Label>
-                <Col sm="10">
-                    <Form.Control type="password" placeholder="Password" onChange={handleUser} />
-                </Col>
-            </Form.Group>
+            
         </>
     );
 }
--- frontend/src/views/LoginView.js
+++ frontend/src/views/LoginView.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 import LoginInput from '../components/LoginInput';
 import LoginForm from '../components/LoginForm';
+import axios from '../axios';
 
 export default function LoginView(){
 
@@ -12,15 +13,31 @@ export default function LoginView(){
 
     const submitUser = (e) => {
         e.preventDefault();
-        console.log(user);
-        history.push("/signup");
+        console.log(user.Password);
+
+        axios({
+            url:'/account/login HTTP/1.1',
+            method:'post',
+            responseType:'json',
+            data:{
+                username: user.Username,
+                password: user.Password
+            }
+        }).then(res => {
+            console.log(res);
+            history.push("/signup");
+        }).catch(err => {
+            console.error(err);
+        }); 
+        
     } 
 
     return (
         <>
             <div>
                 <LoginForm title="Login" onSubmitFunction={submitUser}>
-                   <LoginInput setState={setUser} state={user}></LoginInput> 
+                   <LoginInput setState={setUser} state={user} type="text" placeholder="@username" controlId="userNameLogin" label="Username" ></LoginInput> 
+                   <LoginInput setState={setUser} state={user} type="password" placeholder="Password" controlId="passwordLogin" label="Password" ></LoginInput> 
                 </LoginForm>
             </div>    
         </>
